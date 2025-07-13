3fadbc847c8bb0391f666cd4e10ec3fb3758825a
Athulya <athulyaa175@gmail.com>
2025-07-13 20:58:53 +0530

    namaste

--- Nfn.js
+++ Nfn.js
@@ -1,28 +1,21 @@
-        const radius=[3,1,2,4]
-        const calculateArea=function(radius){
-            const output=[]
-            for(let i=0;i<radius.length;i++){
-                output.push(Math.PI*radius[i]*radius[i])
-            }
-            return output
-        }
-        console.log(calculateArea(radius))
+ const radius=[3,1,2,4]
 
-        const calculateCircumference=function(radius){
+        const area=function(radius){
+            return Math.PI*radius*radius
+        }
+        const circumference=function(radius){
+            return Math.PI*radius
+        }
+        const diameter=function(radius){
+            return 2*radius
+        }
+        const calculate=function(radius,logic){
             const output=[]
             for(let i=0;i<radius.length;i++){
-                output.push(Math.PI*radius[i])
-            }
-            return output
-                
-        }
-        console.log(calculateCircumference(radius))
-
-        const calculateDiameter=function(radius){
-            const output=[]
-            for(let i=0;i<radius.length;i++){
-                output.push(2*radius)
+                output.push(logic(radius[i]))
             }
             return output
         }
-        console.log(calculateCircumference(radius))
+        console.log(calculate(radius,area))
+        console.log(calculate(radius,circumference))
+        console.log(calculate(radius,diameter))
