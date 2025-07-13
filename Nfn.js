f698b31524df66db3094cdf9e526114ebfad8948
Athulya <athulyaa175@gmail.com>
2025-07-13 20:52:11 +0530

    namaste

--- Nfn.js
+++ Nfn.js
@@ -24,3 +24,14 @@
 
     a()
     console.log(b) 
+    
+    setTimeout(function(){
+            console.log("timer")
+        },3000)
+        function x(y){
+            console.log("x")
+        y()
+        }
+        x(function y(){
+            console.log("y")
+        })
