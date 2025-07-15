6afa5029d62b8a849f5bcfd3623a27120898b602
Athulya <athulyaa175@gmail.com>
2025-07-15 23:14:20 +0530

    set

---
+++ Set.js
@@ -0,0 +1,13 @@
+class Person{
+    constructor(firstName,lastName,age){
+        this.firstName=firstName
+        this.lastName=lastName
+        this.age=age
+    }
+}
+  
+const person =new Person(420,69,"pizza")
+console.log(person.firstName)
+console.log(person.lastName)
+console.log(person.age)
+   
