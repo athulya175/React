0a549e5e36093c341add73b96d1b591fbe259298
Athulya <athulyaa175@gmail.com>
2025-07-17 18:36:47 +0530

    Dom

--- DomNavigation.js
+++ DomNavigation.js
@@ -1,4 +1,15 @@
-// 1).firstElementChild
-const element=document.getElementById("Vegetables")
-const firstchild=element.firstElementChild
-firstchild.style.backgroundColor="yellow"
+// ---------------1).firstElementChild--------------
+
+/*
+    const element=document.getElementById("Vegetables")
+    const firstchild=element.firstElementChild
+    firstchild.style.backgroundColor="yellow"
+*/
+
+// to highlight all of the first children
+const allFstchld=document.querySelectorAll("ul")
+allFstchld.forEach(allFstchld=>{
+    const firstchild=allFstchld.firstElementChild
+    firstchild.style.backgroundColor="red"
+})
+
