21f83abd532546c27d7f8a63f5203db3858b412e
Athulya <athulyaa175@gmail.com>
2025-07-18 11:51:26 +0530

    Task

--- DomNavigation.js
+++ DomNavigation.js
@@ -6,10 +6,32 @@
     firstchild.style.backgroundColor="yellow"
 */
 
-// to highlight all of the first children
-const allFstchld=document.querySelectorAll("ul")
-allFstchld.forEach(allFstchld=>{
-    const firstchild=allFstchld.firstElementChild
-    firstchild.style.backgroundColor="red"
-})
 
+/*
+    // to highlight all of the first children
+
+        const allFstchld=document.querySelectorAll("ul")
+        allFstchld.forEach(allFstchld=>{
+            const firstchild=allFstchld.firstElementChild
+            firstchild.style.backgroundColor="red"
+        })
+*/
+
+
+//-------------------2).lastElementChild----------
+/*
+
+    const element=document.getElementById("desserts")
+    const lastchild=element.lastElementChild
+    lastchild.style.backgroundColor="red"
+
+    //highlighting all last element
+
+    const allelement=document.querySelectorAll("ul")
+    allelement.forEach(x=>{
+        const alllastelement=x.lastElementChild
+        alllastelement.style.backgroundColor="red"
+    })
+*/
+
+//------------------------3).nextElementSibling--------
